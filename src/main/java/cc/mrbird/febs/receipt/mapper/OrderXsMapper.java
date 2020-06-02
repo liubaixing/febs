b9167b3ba175f8595c71cc68a05edc9b1419e950
lbx <1363087718@qq.com>
2020-06-02 15:10:57 +0800

    生成销售单mapper

--- src/main/java/cc/mrbird/febs/receipt/mapper/OrderXsMapper.java
+++ src/main/java/cc/mrbird/febs/receipt/mapper/OrderXsMapper.java
@@ -1,7 +1,11 @@
 package cc.mrbird.febs.receipt.mapper;
 
 import cc.mrbird.febs.receipt.entity.OrderXs;
+import cc.mrbird.febs.receipt.entity.OrderXsExample;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 销售单 Mapper
@@ -11,4 +15,26 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface OrderXsMapper extends BaseMapper<OrderXs> {
 
+    long countByExample(OrderXsExample example);
+
+    int deleteByExample(OrderXsExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(OrderXs record);
+
+    int insertSelective(  OrderXs record);
+
+    List<  OrderXs> selectByExample(OrderXsExample example);
+
+      OrderXs selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record")   OrderXs record, @Param("example") OrderXsExample example);
+
+    int updateByExample(@Param("record")   OrderXs record, @Param("example") OrderXsExample example);
+
+    int updateByPrimaryKeySelective(  OrderXs record);
+
+    int updateByPrimaryKey(  OrderXs record);
+    
 }
